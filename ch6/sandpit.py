9408dad174faf85f96192c965886d964ae30fd2d
Jesus Martinez <mtzgtzjesus@gmail.com>
2023-07-26 04:47:21 -0500

    test:

--- ch6/sandpit.py
+++ ch6/sandpit.py
@@ -21,6 +21,14 @@ le.classes_
 
 le.transform(df[1].sample(19))
 
+X_train, X_test, y_train, y_test = \
+    train_test_split(
+        X, y,
+        test_size=0.2,
+        stratify=y,
+        random_state=1, 
+    )
+
 
 
 
